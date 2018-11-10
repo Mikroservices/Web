7f2a05978a86c22f08bcade3c8308db43d1d622f
Marcin Czachurski <mczachurski@icloud.com>
2018-11-10 21:24:12 +0100

    Add validations

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { User } from '../shared/models/user';
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.less']
+    selector: 'app-register',
+    templateUrl: './register.component.html',
+    styleUrls: ['./register.component.less']
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+    user: User = new User();
+    isSubmitting: Boolean = false;
 
-  ngOnInit() {
-  }
+    constructor(private http: HttpClient) { }
 
+    ngOnInit() {
+    }
+
+    async onSubmit() {
+        this.isSubmitting = true;
+        // await this.http.post('', this.user);
+        // browser.driver.sleep(10000);
+
+        await this.delay(3000);
+        this.isSubmitting = false;
+    }
+
+    async delay(ms: number) {
+        return new Promise( resolve => setTimeout(resolve, ms) );
+    }
 }
