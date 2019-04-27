41f4327fab484fffd62a6b5c6327691428cac83d
Marcin Czachurski <mczachurski@icloud.com>
2019-04-27 14:04:34 +0200

    Change addresses

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
     recaptchaKey: '6LfwsncUAAAAAPsUABRdM5OQVLRBFur_ZVQJoXPx',
-    httpSchema: 'http://',
-    usersService: 'users.letterer.me',
-    storiesService: 'stories.letterer.me'
+    httpSchema: 'https://',
+    usersService: 'users.mikroservices.dev',
+    storiesService: 'stories.mikroservices.dev'
 };
